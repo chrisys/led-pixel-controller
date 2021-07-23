09f5a548f8495cc92a436ecd60f1bf89eb279ed2
Chris Crocker-White <chriscw@balena.io>
2021-07-23 11:33:44 +0100

    Update hostname to led-pixel-controller.local
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- vnc/start.sh
+++ vnc/start.sh
@@ -2,5 +2,10 @@
 # Rename the files to allow for default connection html page
 ln -s /noVNC/vnc.html /noVNC/index.html
 
+# Set hostname
+curl -X PATCH --header "Content-Type:application/json" \
+    --data '{"network": {"hostname": "led-pixel-controller"}}' \
+    "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
+
 # Start noVNC
 exec /noVNC/utils/launch.sh --vnc 172.20.0.4:5900 --listen 80
